bebcc0e69edcb7e48fe57868cffd2d2d40e11d81
VGorbovyj <tory.vald@gmail.com>
2017-04-11 11:51:14 +0300

    Update common.js

--- js/common.js
+++ js/common.js
@@ -1,27 +1,3 @@
-$('.tab_1_leb').on('click',function(){
-	$('.tab_1_leb').addClass('active');
-	$('.tab_2_leb').removeClass('active');
-	$('.tab_3_leb').removeClass('active');
-	$('.tab_1_con').addClass('selected');
-  $('.tab_2_con').removeClass('selected');
-  $('.tab_3_con').removeClass('selected');
-});
-$('.tab_2_leb').on('click',function(){
-	$('.tab_2_leb').addClass('active');
-	$('.tab_1_leb').removeClass('active');
-	$('.tab_3_leb').removeClass('active');
-	$('.tab_2_con').addClass('selected');
-  $('.tab_1_con').removeClass('selected');
-  $('.tab_3_con').removeClass('selected');
-});
-$('.tab_3_leb').on('click',function(){
-	$('.tab_3_leb').addClass('active');
-	$('.tab_1_leb').removeClass('active');
-	$('.tab_2_leb').removeClass('active');
-	$('.tab_3_con').addClass('selected');
-  $('.tab_1_con').removeClass('selected');
-  $('.tab_2_con').removeClass('selected');
-});
 $(document).ready(function(){
 	$(".to-the-top").hide();
 	$(function () {
